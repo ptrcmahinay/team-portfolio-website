a482088a99e30547e12e53574ec15078300b96b1
ptrcmahinay <patriciamahinay@outlook.com>
2026-05-10 20:29:27 +0800

    Project initialized, tailwindCSS and shadcn ui integrated

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Button } from '@/components/ui/button'
+
+const App = () => {
+  return (
+    <div>
+      <Button size="lg">Click me</Button>
+    </div>
+  )
+}
+
+export default App
